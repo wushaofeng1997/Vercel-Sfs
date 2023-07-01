855cfca110ee233838455a26a0676623ce11b2a8
CZW <80454999+CZwwww@users.noreply.github.com>
2023-07-01 22:34:21 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,50 +1,19 @@
 package api
 
 import (
-	"io"
 	"log"
 	"net/http"
-	"net/url"
-	"os"
-	"path"
-	"strings"
 	"time"
 )
 
 // Handle Serverless Func
 func Handle(w http.ResponseWriter, r *http.Request) {
-	log.Printf("method: %s path:%s remote:%s", r.Method, r.RequestURI, r.RemoteAddr)
+	log.Printf("method: %s path:%s query:%v remote:%s", r.Method, r.RequestURI, r.URL.Query(), r.RemoteAddr)
 	var start = time.Now()
 	defer func() {
 		log.Printf("method: %s path:%s remote:%s spent:%v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
 	}()
-	parse, err := url.Parse(os.Getenv("PROXY_URL"))
-	if err != nil {
-		write503(w, err)
-		return
-	}
-	log.Println(parse.Scheme)
-	//log.Println(parse.Opaque)
-	request, err := http.NewRequest(r.Method, parse.Scheme+"://"+path.Join(parse.Host, strings.TrimLeft(r.RequestURI,"/api/index")), r.Body)
-	if err != nil {
-		write503(w, err)
-		return
-	}
-	request.Header = r.Header.Clone()
-	request.Form = r.Form
-	request.PostForm = r.PostForm
-	request.MultipartForm = r.MultipartForm
-	client := &http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		write503(w, err)
-		return
-	}
-	for s, vs := range resp.Header {
-		w.Header().Add(s, strings.Join(vs, "; "))
-	}
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
+	w.Write([]byte(r.RequestURI))
 }
 
 func write503(w http.ResponseWriter, err error) {
