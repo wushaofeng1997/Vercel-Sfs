41cdd03ac10664d238a22f7f657b28aad9161626
CZW <80454999+CZwwww@users.noreply.github.com>
2023-07-01 18:43:11 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -2,7 +2,20 @@ package api
 
 import "net/http"
 
+// Handle Serverless Func
 func Handle(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(r.RequestURI))
+	log.Printf("method: %s path:%s remote:%s", r.Method, r.RequestURI, r.RemoteAddr)
+	var start = time.Now()
+	defer func() {
+		log.Printf("method: %s path:%s remote:%s spent:%v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
+	}()
+	getenv := os.Getenv("PROXY_URL")
+	parse, err := url.Parse(getenv)
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	proxy := httputil.NewSingleHostReverseProxy(parse)
+	proxy.ServeHTTP(w, r)
 }
