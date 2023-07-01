745e6fa0b1780eb672791c3464c166a734c8dc9a
CZW <80454999+CZwwww@users.noreply.github.com>
2023-07-01 19:50:30 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"os"
+	"path"
+	"strings"
 	"time"
 )
 
@@ -16,13 +18,36 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		log.Printf("method: %s path:%s remote:%s spent:%v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
 	}()
-	getenv := os.Getenv("PROXY_URL")
-	parse, err := url.Parse(getenv)
+	parse, err := url.Parse(os.Getenv("PROXY_URL"))
 	if err != nil {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte(err.Error()))
+		write503(w, err)
 		return
 	}
-	proxy := httputil.NewSingleHostReverseProxy(parse)
-	proxy.ServeHTTP(w, r)
+	log.Println(parse.Scheme)
+	//log.Println(parse.Opaque)
+	request, err := http.NewRequest(r.Method, parse.Scheme+"://"+path.Join(parse.Host, r.RequestURI), r.Body)
+	if err != nil {
+		write503(w, err)
+		return
+	}
+	request.Header = r.Header.Clone()
+	request.Form = r.Form
+	request.PostForm = r.PostForm
+	request.MultipartForm = r.MultipartForm
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		write503(w, err)
+		return
+	}
+	for s, vs := range resp.Header {
+		w.Header().Add(s, strings.Join(vs, "; "))
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func write503(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusServiceUnavailable)
+	w.Write([]byte(err.Error()))
 }
